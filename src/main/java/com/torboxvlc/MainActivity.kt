13ee231ed082555166aba3df91c98831eb1ed035
starkblast71 <starkblast71@gmail.com>
2026-03-22 12:19:21 +0100

    Update MainActivity.kt

--- src/main/java/com/torboxvlc/MainActivity.kt
+++ src/main/java/com/torboxvlc/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.torboxvlc
 
-import androidx.compose.material3.MaterialTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
